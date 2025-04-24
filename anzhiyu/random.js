818e9f658670a729510e2d323d3446e99a0d6cd3
SouthC <southc@yeah.net>
2025-04-24 21:26:06 +0800

    Site updated: 2025-04-24 21:26:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/19/hello-world/"];function toRandomPost(){
+var posts=["2025/04/24/APK正则去广告法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
