93bfa11cb91e102e5a07dd86a379576acd02c333
SouthC <southc@yeah.net>
2025-04-26 12:50:25 +0800

    Site updated: 2025-04-26 12:50:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/26/如何修改海量广告的软件？/","2025/04/25/欢迎进入我的网站！/"];function toRandomPost(){
+var posts=["2025/04/25/欢迎进入我的网站！/","2025/04/26/如何修改海量广告的软件？/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
