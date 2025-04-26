e7413752dbfbb88921fe820b644255bc84ead862
SouthC <southc@yeah.net>
2025-04-26 20:48:46 +0800

    Site updated: 2025-04-26 20:48:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/26/一个步骤使你在hexo-d后不再打开网页/","2025/04/26/Re-从零开始了解东方Project/","2025/04/26/如何修改海量广告的软件？/","2025/04/25/欢迎进入我的网站！/"];function toRandomPost(){
+var posts=["2025/04/26/Re-从零开始了解东方Project/","2025/04/26/一个步骤使你在hexo-d后不再打开网页/","2025/04/26/如何修改海量广告的软件？/","2025/04/25/欢迎进入我的网站！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
